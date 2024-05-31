482b16ac284424e1049ee92dde10e29cf7ac953f
zena3 <35606005+zena3@users.noreply.github.com>
2024-05-31 21:43:48 +0200

    reworked the part on importing the variables

--- config.py
+++ config.py
@@ -1,30 +1,9 @@
 import json
 
-class Config:
-    def __init__(self, config_file="config/settings.json"):
-        with open(config_file) as f:
-            self.settings = json.load(f)
+def load_settings(file_path):
+    with open(file_path, 'r') as f:
+        settings = json.load(f)
+    return settings
 
-    @property
-    def threshold_temp(self):
-        return self.settings["threshold_temp"]
-
-    @property
-    def email_settings(self):
-        return self.settings["email"]
-
-    @property
-    def fixed_values(self):
-        return self.settings["fixed_values"]
-
-    @property
-    def batch_number(self):
-        return self.settings["batch_number"]
-
-    @property
-    def time_interval(self):
-        return int(self.settings["time_interval"])  # Ensure it's an integer
-       
-    @property
-    def data_base(self):
-        return self.settings["data_base.txt"]
+def get_setting(settings, key):
+    return settings.get(key)
--- monitor.py
+++ monitor.py
@@ -1,13 +1,16 @@
 import os
 import time
 import Adafruit_DHT
-from config import Config #import from settings.json the configurations for the monitoring process
+import config #import from settings.json the configurations for the monitoring process
 
-#import and set the values from settings.json
-threshold_temp = Config.threshold_temp
-batch_number = Config.batch_number
-time_interval = Config.time_interval
-data_base = Config.data_base
+# Load settings from JSON file
+settings = config.load_settings('settings.json')
+
+# Extract settings values
+data_base = config.get_setting(settings, 'data_base')
+threshold_temp = config.get_setting(settings, 'threshold_temp')
+batch_number = config.get_setting(settings, 'batch_number')
+time_interval = config.get_setting(settings, 'time_interval')
 
 #set the pin for the DHT22 sensor to read from 
 DHT_SENSOR = Adafruit_DHT.DHT22
@@ -16,7 +19,7 @@ DHT_PIN = 7
 #Open a txt file called "data_base.txt", if it's already in your folder, if not, create the file with the headers for all the sensor readings
 #Define the headers of the data_base
 data_base = "data_base.txt"
-headers = ["date", "time", "temperature"]
+headers = ["date", "time", "temperature","humidity", "threshold_temp", "batch_number"]
 #time interval = 2
 
 # Check if the file exists
@@ -26,18 +29,14 @@ if not os.path.exists(data_base):
     with open(data_base, "w") as f:
         f.write(",".join(headers) + "\n")
         print(f"File {data_base} created and appending values to it.")
-
-#add the readings of the sensor to the database
-f = open(data_base, 'a')
-
-#set the time interval for the sensor to read the temperature and humidity  
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    #register temperature, humidity, and the time (hour and minute)
-    if humidity is not None and temperature is not None:
-        f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity, threshold_temp))
-        print("{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n".format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity, threshold_temp ))
-    else:
-        print("Failed to retrieve data from humidity sensor")
-    #set time in seconds for every n number of seconds you want to register a temperature reading
-    time.sleep(time_interval)
+        #set the time interval for the sensor to read the temperature and humidity  
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        #register temperature, humidity, and the time (hour and minute)
+        if humidity is not None and temperature is not None:
+            f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity, threshold_temp, batch_number))
+            print("{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n".format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity, threshold_temp, batch_number))
+        else:
+            print("Failed to retrieve data from humidity sensor")
+        #set time in seconds for every n number of seconds you want to register a temperature reading
+        time.sleep(time_interval)
