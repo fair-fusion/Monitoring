140979f9233eb3d0f36409694c32d10dab9d51f8
zena3 <35606005+zena3@users.noreply.github.com>
2024-05-31 16:40:46 +0200

    some stuff I forgot to adjust in the config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
             self.settings = json.load(f)
 
     @property
-    def threshold(self):
+    def threshold_temp(self):
         return self.settings["threshold_temp"]
 
     @property
@@ -20,3 +20,11 @@ class Config:
     @property
     def batch_number(self):
         return self.settings["batch_number"]
+    
+    @property
+    def time_interval(self):
+        return self.settings["time_interval"]
+    
+    @property
+    def data_base(self):
+        return self.settings["data_base"]
