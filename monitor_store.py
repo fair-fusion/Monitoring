3a46a12e3763ed037f525fc1727e1e5bf7f5a095
zena3 <35606005+zena3@users.noreply.github.com>
2024-06-30 12:16:49 +0200

    updated monitor_store.py to register readings in a csv file

--- monitor_store.py
+++ monitor_store.py
@@ -1,39 +1,34 @@
-#source: https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup
-
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_dht
+import os
+import csv
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D5)
-time_interval = 2.0
+time_interval = 30.0
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+# Check if the file exists, create it if it doesn't
+if not os.path.exists("data.csv"):
+    with open("data.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Time", "Temperature (C)", "Humidity (%)"])
 
 while True:
     try:
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        f = open(file_name, 'a+')
-        if os.stat(file_name).st_size == 0:
-            f.write('Date,Time,Temperature,Humidity, Heating Status, Water Supply Status\r\n')
+        current_time = time.strftime("%H:%M:%S")
         # Print the values to the serial port
-        print(
-            "Temp: {:.1f} C    Humidity: {}% ".format(
-                temperature_c, humidity
-            )
-        )
-
+        print(f"Temp: {temperature_c:.1f} C    Humidity: {humidity}%    Time: {current_time}")
+        # Write to CSV file
+        with open("data.csv", "a", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([current_time, temperature_c, humidity])
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(time_interval)
         continue
     except Exception as error:
         dhtDevice.exit()
