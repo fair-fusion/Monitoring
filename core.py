5ba482371dd49734cd437bbaf7b85d2209af96bc
zena3 <35606005+zena3@users.noreply.github.com>
2024-08-20 20:54:01 +0200

    update to display script to separate the date_time column

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-#all the necessary functions to monitor temperature and send notifcations. This comes together in monitor_store.py 
+#all the necessary functions to monitor temperature and send notifications. This comes together in monitor_store.py 
 import time
 import board
 import adafruit_dht
@@ -41,8 +41,7 @@ class TemperatureMonitor:
         try:
             temperature_c = self.dhtDevice.temperature
             humidity = self.dhtDevice.humidity
-            current_time = time.strftime("YYYY-MM-DD HH:MM:SS")
-            #current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print(f"Temp: {temperature_c:.1f} C    Humidity: {humidity}%    Time: {current_time}")
             return temperature_c, humidity, current_time
         except RuntimeError as error:
